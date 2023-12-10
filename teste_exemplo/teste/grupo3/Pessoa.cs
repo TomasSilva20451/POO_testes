eb5c3e54424bb143c759d4a1c620b97698ec9259
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-10 23:09:58 +0000

    GRUPO III (3)
    GRUPO III
    A ADOPÇÃO ADEQUADA DAS NORMAS CLS CORRESPONDE A 1 VALOR
    (7 + 1 valores – 60 min)
    Pretende-se desenvolver um sistema capaz de monitorizar as visitas de pessoas a um
    determinado museu. Considere-se para isso que:
    i) Cada pessoa é identificada pelo respetivo nome, NC, data de nascimento, localidade
    ii) Cada pessoa só poderá fazer uma visita por ano a esse museu.
    Posto isto, serão necessários múltiplos serviços capazes de, por exemplo:
    • Visitar o museu.
    • Consultar dados da pessoa.
    • Consultar as pessoas que já visitaram o museu.
    1. Desenvolva o conjunto de classes que considere pertinentes para a implementação deste sistema,
    assim como as estruturas de dados mais adequadas para gerir as pessoas que visitam o museu.
    2. Implementar os seguintes métodos:
    a. VisitaMuseu, para registar a visita ao museu de uma pessoa, num determinado dia;
    b. PodeVisitar, para verificar se determinada pessoa pode ou não visitar um
    determinado museu.
    3. Implemente um método capaz de guardar em ficheiro toda a informação considerada pertinente;
    4. Desenvolva uma pequena aplicação que demonstre a utilização de todos os métodos
    desenvolvidos.

---
+++ teste_exemplo/teste/grupo3/Pessoa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Localidade { get; set; }
+    public List<DateTime> Visitas { get; set; }
+
+    public Pessoa(string nome, DateTime dataNascimento, string localidade)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        Localidade = localidade;
+        Visitas = new List<DateTime>();
+    }
+}
+
+public class Museu
+{
+    private List<Pessoa> pessoas;
+
+    public Museu()
+    {
+        pessoas = new List<Pessoa>();
+    }
+
+    public void VisitaMuseu(Pessoa pessoa, DateTime dataVisita)
+    {
+        if (!pessoa.Visitas.Exists(v => v.Year == dataVisita.Year))
+        {
+            pessoa.Visitas.Add(dataVisita);
+            Console.WriteLine($"{pessoa.Nome} visitou o museu em {dataVisita.ToShortDateString()}");
+        }
+        else
+        {
+            Console.WriteLine($"{pessoa.Nome} já visitou o museu este ano.");
+        }
+    }
+
+    public bool PodeVisitar(Pessoa pessoa)
+    {
+        return !pessoa.Visitas.Exists(v => v.Year == DateTime.Now.Year);
+    }
+
+    public void GuardarEmFicheiro(string caminhoFicheiro)
+    {
+        using (StreamWriter sw = new StreamWriter(caminhoFicheiro))
+        {
+            foreach (var pessoa in pessoas)
+            {
+                sw.WriteLine($"{pessoa.Nome},{pessoa.DataNascimento.ToShortDateString()},{pessoa.Localidade}");
+            }
+        }
+    }
+}
+
+
---
+++ teste_exemplo/teste/grupo3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace grupo3;
+
+class Program
+{
+    static void Main()
+    {
+        Museu museu = new Museu();
+
+        Pessoa pessoa1 = new Pessoa("João", new DateTime(1990, 5, 15), "Lisboa");
+        Pessoa pessoa2 = new Pessoa("Maria", new DateTime(1985, 8, 25), "Porto");
+
+        museu.VisitaMuseu(pessoa1, new DateTime(2023, 7, 10));
+        museu.VisitaMuseu(pessoa2, new DateTime(2023, 6, 5));
+        museu.VisitaMuseu(pessoa1, new DateTime(2023, 8, 20));
+
+        if (museu.PodeVisitar(pessoa1))
+        {
+            Console.WriteLine($"{pessoa1.Nome} pode visitar o museu.");
+        }
+        else
+        {
+            Console.WriteLine($"{pessoa1.Nome} não pode visitar o museu este ano.");
+        }
+
+        museu.GuardarEmFicheiro("pessoas.txt");
+    }
+}
