dd8e34da4aa8ea6b2fa78a6f4f4d634e8eb3409e
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-10 23:03:37 +0000

    Grupo II
    Considere um dicionário de tuplos <char,Aluno> que armazena a informação dos alunos de um
    determinado curso, uma escola. A chave (char) corresponde ao primeiro caracter do nome do
    aluno. Posto isto desenvolva um método que devolva todos os alunos que tenham tido aprovação
    a uma determinada unidade curricular, num determinado curso e numa determinada época.
    Coloque a implementação que desenvolver numa biblioteca.

---
+++ teste_exemplo/teste/MeuProjetoDeTeste/Program.cs
@@ -0,0 +1,36 @@
+﻿//MeuProjetoDeTeste/Program.cs
+
+using System;
+using System.Collections.Generic; // Adicione esta linha
+using MinhaBiblioteca;
+
+
+namespace MeuProjetoDeTeste
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Adicionar alguns alunos para teste
+            Escola escola = new Escola();
+            escola.AdicionarAluno('A', new Aluno("Alice", "Engenharia", "Matemática", "Normal", true));
+            escola.AdicionarAluno('B', new Aluno("Bob", "Engenharia", "Matemática", "Normal", false));
+            escola.AdicionarAluno('C', new Aluno("Charlie", "Ciência da Computação", "Programação", "Recurso", true));
+            escola.AdicionarAluno('D', new Aluno("David", "Ciência da Computação", "Programação", "Normal", true));
+
+
+            // Obter alunos aprovados em "Matemática" para o curso "Engenharia" na época "Normal"
+            List<Aluno> aprovados = escola.ObterAlunosAprovados("Matemática", "Engenharia", "Normal");
+
+            // Exibir resultados
+            Console.WriteLine("Alunos Aprovados:");
+            foreach (var aluno in aprovados)
+            {
+                Console.WriteLine($"{aluno.Nome} - Curso: {aluno.Curso}, Época: {aluno.Epoca}");
+            }
+
+            // Aguardar pressionar Enter antes de fechar a console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ teste_exemplo/teste/MinhaBiblioteca/Aluno.cs
@@ -0,0 +1,55 @@
+﻿//MinhaBiblioteca/Aluno.cs
+
+using System;
+using System.Collections.Generic;
+
+namespace MinhaBiblioteca
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public string UnidadeCurricular { get; set; }
+        public string Epoca { get; set; }
+        public bool Aprovado { get; set; }
+
+        // Construtor que inicializa as propriedades obrigatórias
+        public Aluno(string nome, string curso, string unidadeCurricular, string epoca, bool aprovado)
+        {
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Curso = curso ?? throw new ArgumentNullException(nameof(curso));
+            UnidadeCurricular = unidadeCurricular ?? throw new ArgumentNullException(nameof(unidadeCurricular));
+            Epoca = epoca ?? throw new ArgumentNullException(nameof(epoca));
+            Aprovado = aprovado;
+        }
+    }
+
+
+    public class Escola
+    {
+        private Dictionary<char, Aluno> alunos = new Dictionary<char, Aluno>();
+
+        public void AdicionarAluno(char chave, Aluno aluno)
+        {
+            alunos[chave] = aluno;
+        }
+
+        public List<Aluno> ObterAlunosAprovados(string unidadeCurricular, string curso, string epoca)
+        {
+            List<Aluno> aprovados = new List<Aluno>();
+
+            foreach (var aluno in alunos.Values)
+            {
+                if (aluno.UnidadeCurricular == unidadeCurricular &&
+                    aluno.Curso == curso &&
+                    aluno.Epoca == epoca &&
+                    aluno.Aprovado)
+                {
+                    aprovados.Add(aluno);
+                }
+            }
+
+            return aprovados;
+        }
+    }
+}
