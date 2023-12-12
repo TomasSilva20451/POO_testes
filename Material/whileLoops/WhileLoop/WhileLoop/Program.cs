093adaecbe26537fb62fa7055d400e7d6df360cb
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-12 11:09:53 +0000

    While Loops

---
+++ Material/whileLoops/WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace WhileLoop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int index = 1;
+        while (index <= 5) 
+        {
+            Console.WriteLine(index);
+            index++; // incrementar 1;
+        }
+
+
+
+        Console.ReadLine();
+    }
+}
+
