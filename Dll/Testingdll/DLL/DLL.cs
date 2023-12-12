5a14b0b8210e106e5149b9e0e217c62436684fed
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-12 11:13:12 +0000

    DLL

---
+++ Dll/Testingdll/DLL/DLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DLL;
+
+public static class DLL
+{
+    public static int Divide(int x, int y)
+    {
+        if (y == 0)
+        {
+            throw new DivideByZeroException();
+        }
+
+       return x / y;
+    }
+}
+
