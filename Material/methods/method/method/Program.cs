1a4d960dcb8f3fc80d94d5ec7bb7a604935eb176
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-12 11:10:35 +0000

    Method

---
+++ Material/methods/method/method/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace method;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayHi("Tomas", "Silva");
+
+        Console.ReadLine();
+    }
+
+    static void SayHi(string name, string apelido)
+    {
+        
+        Console.WriteLine("Hello " + name + " " +  apelido);
+    }
+
+}
+
