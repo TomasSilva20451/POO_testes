893f97ed4c26c29d19c703e9329f2911a557e18d
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-12 11:13:58 +0000

    Arrays

---
+++ Material/arrays/array/array/array/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] luckyNumbers = { 4, 8, 16, 32, 64, 128, 254 };
+        string[] friends = new string[5];// array nova com 5 elementos
+
+        friends[0] = "Gin";
+        friends[1] = "Kayla";
+        friends[2] = "Tommy";
+        friends[3] = "Telmo";
+        friends[4] = "John";
+
+
+        luckyNumbers[2] = 900;
+
+        Console.WriteLine(luckyNumbers[2] + " " + friends[2]);
+
+        Console.ReadLine();
+    }
+}
+
