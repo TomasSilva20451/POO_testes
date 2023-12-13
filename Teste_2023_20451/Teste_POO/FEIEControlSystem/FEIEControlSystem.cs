b2637887cf64e9a6d7ed2c2d5a1b02098a85ea94
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-13 14:53:42 +0000

    Teste POO ESI 2023_2024
    Teste_POO_ESI_2023_2024

---
+++ Teste_2023_20451/Teste_POO/FEIEControlSystem/FEIEControlSystem.cs
@@ -0,0 +1,126 @@
+﻿/**
+ * @file FEIEControlSystem.cs
+ * @author Tomás (a20451@alunos.ipca.pt)
+ * @brief Definição da classe FEIEControlSystem
+ * @version 0.1
+ * @date 2023-12-12
+ * @copyright Copyright (c) 2023
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FEIEControlSystem
+{
+    #region Classes
+
+    /// <summary>
+    /// Representa o sistema de controlo do Fundo Europeu para Investimentos Estratégicos (FEIE).
+    /// </summary>
+    public class FEIEControlSystem
+    {
+        #region Atributos
+
+        private List<Country> countries;
+        private List<Funding> fundings;
+
+        #endregion
+
+        #region Construtores
+
+        /// <summary>
+        /// Construtor padrão da classe FEIEControlSystem.
+        /// </summary>
+        public FEIEControlSystem()
+        {
+            countries = new List<Country>();
+            fundings = new List<Funding>();
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// Registar o montante do FEIE executado, num determinado ano, para um determinado país.
+        /// </summary>
+        /// <param name="nomePais">Nome do país.</param>
+        /// <param name="ano">Ano do registro.</param>
+        /// <param name="montanteExecutado">Montante a ser registrado.</param>
+        public void RegistarMontanteExecutado(string nomePais, int ano, decimal montanteExecutado)
+        {
+            Country pais = countries.FirstOrDefault(p => p.Nome == nomePais);
+
+            if (pais != null)
+            {
+                Funding financiamento = fundings.FirstOrDefault(f => f.Ano == ano && f.Pais == pais);
+
+                if (financiamento != null)
+                {
+                    financiamento.MontanteExecutado = montanteExecutado;
+                }
+                else
+                {
+                    AdicionarFinanciamento(new Funding(ano, 0, montanteExecutado, pais ?? throw new InvalidOperationException("O país não pode ser nulo.")));
+                }
+            }
+            else
+            {
+                IIOServices ioService = new ConsoleIOServices();
+                if (ioService != null)
+                {
+                    ioService.WriteLine($"País '{nomePais}' não encontrado.");
+                    string userInput = ioService.ReadLine();
+                }
+            }
+        }
+
+        // Método para calcular a percentagem a executar num determinado ano
+        public decimal CalcularPercentagemAExecutar(int ano)
+        {
+            // Lógica de cálculo da percentagem aqui
+            return 0.0m; // Substitua pela lógica real
+        }
+
+        /// <summary>
+        /// Identificar os países que estão atrasados na execução do respetivo FEIE.
+        /// </summary>
+        /// <returns>Lista de países em atraso.</returns>
+        public List<Country> PaisesAtrasados()
+        {
+            return countries.Where(p => p != null && !p.ExecucaoNoPrazo()).ToList();
+        }
+
+        /// <summary>
+        /// Ordenar os países pelo montante executado do respetivo FEIE.
+        /// </summary>
+        /// <returns>Lista de países ordenados pelo montante executado.</returns>
+        public List<Country> OrdenarPaisesPorMontanteExecutado()
+        {
+            return countries.Where(p => p != null).OrderByDescending(p => p.MontanteExecutado).ToList();
+        }
+
+        /// <summary>
+        /// Adicionar um financiamento à lista de financiamentos.
+        /// </summary>
+        /// <param name="financiamento">Financiamento a ser adicionado.</param>
+        public void AdicionarFinanciamento(Funding financiamento)
+        {
+            fundings.Add(financiamento);
+        }
+
+        /// <summary>
+        /// Adicionar um país à lista de países.
+        /// </summary>
+        /// <param name="country">País a ser adicionado.</param>
+        public void AdicionarPais(Country country)
+        {
+            countries.Add(country);
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ Teste_2023_20451/Teste_POO/Grupo1/Veiculo.cs
@@ -0,0 +1,87 @@
+﻿/**
+ * @file Veiculo.cs
+ * @author Tomás (a20451@alunos.ipca.pt)
+ * @brief Definição da classe Veiculo
+ * @version 0.1
+ * @date 2023-12-12
+ * @copyright Copyright (c) 2023
+ */
+
+using System;
+
+public class Veiculo
+{
+    #region Atributos
+
+    public string Tipo { get; set; }
+    public int Cilindrada { get; set; }
+    public int AnoAquisicao { get; set; }
+    public bool Avariado { get; set; }
+
+    #endregion
+
+    #region Construtores
+
+    /// <summary>
+    /// Construtor padrão da classe Veiculo.
+    /// </summary>
+    public Veiculo()
+    {
+        Tipo = "Desconhecido"; // Valor padrão para Tipo
+    }
+
+    /// <summary>
+    /// Construtor parametrizado da classe Veiculo.
+    /// </summary>
+    /// <param name="tipo">Tipo do veículo.</param>
+    /// <param name="cilindrada">Cilindrada do veículo.</param>
+    /// <param name="anoAquisicao">Ano de aquisição do veículo.</param>
+    /// <param name="avariado">Indica se o veículo está avariado.</param>
+    public Veiculo(string tipo, int cilindrada, int anoAquisicao, bool avariado)
+    {
+        // Atribua um valor padrão ou um valor válido ao Tipo
+        Tipo = tipo ?? throw new ArgumentNullException(nameof(tipo), "O tipo do veículo não pode ser nulo.");
+        Cilindrada = cilindrada;
+        AnoAquisicao = anoAquisicao;
+        Avariado = avariado;
+    }
+
+
+    #endregion
+
+    #region Propriedades
+
+    // Propriedades já definidas anteriormente.
+
+    #endregion
+
+    #region Métodos
+
+    // Métodos da classe, se houver.
+
+    #endregion
+
+    #region Overrides
+
+    // Overrides de métodos da classe base, se houver.
+
+    #endregion
+
+    #region Outros Métodos
+
+    // Outros métodos da classe, se houver.
+
+    #endregion
+
+    #region Destrutores
+
+    /// <summary>
+    /// Destrutor da classe Veiculo.
+    /// </summary>
+    ~Veiculo()
+    {
+        // Lógica de destruição, se necessário.
+    }
+
+    #endregion
+}
